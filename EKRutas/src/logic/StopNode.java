259b9ee422b007315b96fa8722896c44b8858f57
eliasdlc <exdj0002@ce.pucmm.edu.do>
2024-11-02 13:04:19 -0400

    removeRoute and removeStopNode functions added.

--- EKRutas/src/logic/StopNode.java
+++ EKRutas/src/logic/StopNode.java
@@ -29,6 +29,31 @@ public class StopNode {
 		listRoutes.get(dest).add(route);
 	}
 	
+	public void removeRoute(StopNode dest, Ruta route) {
+		List<Ruta> routes = listRoutes.get(dest);
+		
+		if ( routes != null ) { 
+			routes.remove(route);
+			
+			if ( routes.isEmpty() ){ //if there's nothing left in the list then the destination gets removed.
+				listRoutes.remove(dest);
+			}
+		}
+	}
+	
+	public void removeStopNode(StopNode node) {
+		node.getAdjacentDestinations().forEach(destination -> node.removeRoute(destination, null));
+		
+		// Remove all the references of this node to other nodes
+		for ( StopNode otherNode : listRoutes.keySet() ) { 
+			otherNode.getAdjacentDestinations().forEach(destination -> {
+				if (destination.equals(node)) {
+					otherNode.removeRoute(node, null);
+				}
+			});
+		}
+	}
+	
 	public List<StopNode> getAdjacentDestinations(){
 		return new ArrayList<>(listRoutes.keySet());
 	} 
